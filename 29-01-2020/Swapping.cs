ab0fff5e9e784828af2352d55c3c19c78844576b
Rohith Reddy MT <47552290+rohithreddymt@users.noreply.github.com>
2020-02-07 11:40:12 +0530

    Add files via upload

---
+++ 29-01-2020/Swapping.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace numberswapping
+{
+    class Swapping
+    {
+        static void Main(string[] args)
+        {
+			int number1, number2, temporary;
+            Console.Write("\nNumber1 : ");
+			number1 = int.Parse(Console.ReadLine());
+            Console.Write("\nNumber2 : ");
+			number2 = int.Parse(Console.ReadLine());
+
+			temporary = number1;
+			number1 = number2;
+			number2 = temporary;
+
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nNumber1 : " + number1);
+            Console.Write("\nNumber2 : " + number2);
+			Console.ReadKey();
+			
+		}
+	}
+}
